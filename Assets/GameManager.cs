d4db3e228dcfb8c7dfd5b521af1a647314410aef
wakaba0972 <benny.wu2199@gmail.com>
2025-05-22 21:12:16 +0800

    更新ScriptManager
    用輪詢機制定期向Server檢查有無新文本，若有則下載並儲存

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,8 @@
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Android;
+using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +14,18 @@ public class GameManager : MonoBehaviour
     private GameObject star;
     private GameObject squidward;
     private GameObject krab;
+
+    // 已完成的劇本數(ID)
+    private int Executable_ID;
+
+    // 已儲存文本的劇本數(ID)
+    private int Script_ID;
+
+    // 已儲存TTS的劇本數(ID)  
+    private int TTS_ID;
+
+    // 用於停止請求的標誌
+    private bool Stop = false;
 
 
     void Awake()
@@ -28,10 +42,19 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        // 獲取角色的GameObject，方便控制
         sponge = GameObject.Find("Spongebob wrapper");
         star = GameObject.Find("Patrick wrapper");
         squidward = GameObject.Find("Squidward wrapper");
         krab = GameObject.Find("Krab wrapper");
+
+        // 從 PlayerPrefs 讀取Counter, 用作獲取劇本的編號
+        Executable_ID = PlayerPrefs.GetInt("Executable_ID", 0);
+        Script_ID = PlayerPrefs.GetInt("Script_ID", 0);
+        TTS_ID = PlayerPrefs.GetInt("TTS_ID", 0);
+
+        // 背景執行Script_Request_Loop()
+        _ = Script_Request_Loop();
     }
 
     void Update()
@@ -42,5 +65,34 @@ public class GameManager : MonoBehaviour
     private void  New_Round()
     {
 
+    }
+
+    private async Task Script_Request_Loop() {
+        while (!Stop)
+        {
+            Debug.Log("執行Script Request!");
+
+            // 更新Script_ID, 向Server請求劇本並儲存至本地端
+            Script_ID = await scriptManager.Request(Script_ID);
+
+            // 每10秒請求一次
+            await Task.Delay(10000);
+        }
+    }
+    
+    private void OnDestroy()
+    {
+        /*// 程式結束時儲存當前的Counter
+        PlayerPrefs.SetInt("Executable_ID", Executable_ID);
+        PlayerPrefs.SetInt("Script_ID", Script_ID);
+        PlayerPrefs.SetInt("TTS_ID", TTS_ID);*/
+
+        // 測試用
+        PlayerPrefs.SetInt("Executable_ID", 0);
+        PlayerPrefs.SetInt("Script_ID", 0);
+        PlayerPrefs.SetInt("TTS_ID", 0);
+
+        // 中止請求
+        Stop = true;
     }
 }
--- Assets/ScriptManager.cs
+++ Assets/ScriptManager.cs
@@ -1,51 +1,103 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using System.Threading.Tasks;
+using System;
+using UnityEditor.PackageManager.Requests;
+using System.IO;
 
 
 public class ScriptManager : MonoBehaviour
 {
-    private int MAX_ID;
-    private int Counter;
-    private string URL_SCRIPT  = "http://localhost:3000/scripts";
-    private string URL_MAX = "http://localhost:3000/max";
+    private string SAVE_PATH;
+    private const string URL_SCRIPT  = "http://localhost:3000/scripts";
+    private const string URL_MAX = "http://localhost:3000/max";
 
-    void Start()
+    private void Start()
     {
-        // 從 PlayerPrefs 讀取Counter, 用作獲取劇本的編號
-        //Counter = PlayerPrefs.GetInt("Counter", 0);
+        SAVE_PATH = Application.persistentDataPath;
+        Debug.Log($"檔案將儲存在 {SAVE_PATH}");
     }
 
-    private void OnDestroy()
+    // 儲存劇本為json
+    private void SaveScript(int id, string script)
     {
-        PlayerPrefs.SetInt("Counter", Counter);
+        try {
+            // 建立資料夾 /[id]/
+            string dirPath = $"{SAVE_PATH}/{id}";
+            Directory.CreateDirectory(dirPath);
+
+            // 儲存劇本
+            string fileName = $"{id}.json";
+            string filePath = Path.Combine($"{dirPath}/", fileName);
+            File.WriteAllText(filePath, script);
+            Debug.Log($"儲存劇本於: {filePath}");
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"儲存失敗: {e.Message}");
+        }
     }
 
-    public async void say()
+    // 利用Server的/scripts路由獲取劇本文本
+    // 最後返回劇本的文本(string)
+    private async Task<string> RequestScript(int id)
     {
-        Debug.Log(await RequestMaxID());
+        try {
+            string URL = $"{URL_SCRIPT}?id={id}";
+            using (UnityWebRequest request = UnityWebRequest.Get(URL))
+            {
+                await request.SendWebRequest();
+                Debug.Log($"取得{id}.json文本:");
+
+                return request.downloadHandler.text;
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"請求失敗: {e.Message}");
+            return string.Empty;
+        }
     }
 
-    // 呼叫RequestMaxID()獲取當前最大的ID並設定為MAX_ID
-    public async void SetMaxID()
+    // 獲取MaxID後, 與當前的Script_ID進行比較
+    // 請求[Script_ID+1, MAXID]的文本, 並儲存至本地端
+    // 最後返回MaxID以更新Script_ID
+    public async Task<int> Request(int Current_Script_ID)
     {
-        MAX_ID = await RequestMaxID();
+        try
+        {
+            int maxID = await RequestMaxID();
+            for (int id = Current_Script_ID + 1; id <= maxID; ++id)
+            {
+                string script = await RequestScript(id);
+                SaveScript(id, script);
+            }
+
+            return maxID;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"請求失敗: {e.Message}");
+            return Current_Script_ID;
+        }
     }
 
-    public void RequestScript()
-    {
-
-    }
-
-    // 我放棄coroutine啦!!!
     // 利用Server的/max路由獲取當前最大的ID
     private async Task<int> RequestMaxID()
     {
+        try {
+            using (UnityWebRequest request = UnityWebRequest.Get(URL_MAX))
+            {
+                await request.SendWebRequest();
+                Debug.Log("取得MAX_ID " + request.downloadHandler.text);
 
-        using (UnityWebRequest request = UnityWebRequest.Get(URL_MAX))
+                return int.Parse(request.downloadHandler.text);
+            }
+        }
+        catch (Exception e)
         {
-            await request.SendWebRequest();
-            return int.Parse(request.downloadHandler.text);
+            Debug.LogError($"請求失敗: {e.Message}");
+            return 0;
         }
     }
 }
